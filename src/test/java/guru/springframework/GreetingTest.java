a92fa60c69c9c20f55a3467b8df9232f83495e92
sunny <sunny.keila24@gmail.com>
2021-01-23 23:22:41 +0100

    first test

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,21 +1,44 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.Test;
-
-
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
+    private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("in the BeforeAll ------");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("-------------------");
+        greeting  = new Greeting();
+    }
+
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld());
     }
 
     @Test
     void helloWorld1() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld("sunny"));
+    }
+    @Test
+    void helloWorld2() {
+        System.out.println(greeting.helloWorld("sunny"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("------AfterEach");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("in the AfterAll ------");
     }
 
 }
